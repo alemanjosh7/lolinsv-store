c12cfddbb5e7c7f974bf14d148f805c9f78f6f7a
alemanjosh7 <josuealeman2003@gmail.com>
2022-03-09 16:12:39 -0600

    trabajando index

--- js/index.js
+++ js/index.js
@@ -1,12 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var instance = M.Carousel.init({
-    fullWidth: true,
-  });
-  var instances = M.Carousel.init(document.querySelectorAll(".carousel"));
-});
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// Or with jQuery
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-$(document).ready(function () {
-  $(".carousel").carousel();
-});
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  showSlides((slideIndex = n));
+}
+//Auto slide
+var slideIndex = 0;
+showSlides();
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
