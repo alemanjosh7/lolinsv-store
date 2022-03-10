1ea30b2b114089ecb88fe9f92c4f01e2200e0089
alemanjosh7 <josuealeman2003@gmail.com>
2022-03-10 16:43:42 -0600

    terminando páginas

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 var slideIndex = 1;
 showSlides(slideIndex);
-
 function plusSlides(n) {
   showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -24,70 +19,46 @@ function showSlides(n) {
 
   slides[slideIndex - 1].style.display = "block";
 
-  showSlides((slideIndex = n));
-}
-//Auto slide
-var slideIndex = 0;
-showSlides();
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000);
+  setTimeout(showSlides, 2000);
 }
 /*Estilo de las opciones de los carritos y el navbar mobile*/
 var opcionesCarrito = {
-    edge: 'right'
-}
+  edge: "right",
+};
 var navbarmobile = {
-    edge: 'left'
-}
+  edge: "left",
+};
 //Inicializando componentes de Materialize
-document.addEventListener('DOMContentLoaded', function () {
-    M.Sidenav.init(document.querySelectorAll('.sidenav'));
-    M.Sidenav.init(document.querySelectorAll('#mobile-demo'), navbarmobile);
-    M.Sidenav.init(document.querySelectorAll('#carrito'), opcionesCarrito);
-    M.Slider.init(document.querySelectorAll('.slider'));
-    M.Carousel.init(document.querySelectorAll('.carousel'));
-    M.Tooltip.init(document.querySelectorAll('.tooltipped'));
-    M.FormSelect.init(document.querySelectorAll('select'));
-    M.Modal.init(document.querySelectorAll('.modal'));
+document.addEventListener("DOMContentLoaded", function () {
+  M.Sidenav.init(document.querySelectorAll(".sidenav"));
+  M.Sidenav.init(document.querySelectorAll("#mobile-demo"), navbarmobile);
+  M.Sidenav.init(document.querySelectorAll("#carrito"), opcionesCarrito);
+  M.Slider.init(document.querySelectorAll(".slider"));
+  M.Carousel.init(document.querySelectorAll(".carousel"));
+  M.Tooltip.init(document.querySelectorAll(".tooltipped"));
+  M.FormSelect.init(document.querySelectorAll("select"));
+  M.Modal.init(document.querySelectorAll(".modal"));
 });
 /*Copiar número de Whatsaap en el Footer*/
 function copiarWhat() {
-    var content = document.getElementById('copywhat').innerHTML;
-    navigator.clipboard.writeText(content)
+  var content = document.getElementById("copywhat").innerHTML;
+  navigator.clipboard.writeText(content);
 }
 /*Ocultar navbar mobile tras aparecer carrito*/
-var btnabrircarrito = document.getElementById('abrircarrito-mobile');
-btnabrircarrito.addEventListener('click', function () {
-    let navmobile = M.Sidenav.getInstance(document.querySelector('#mobile-demo'));
-    navmobile.close();
+var btnabrircarrito = document.getElementById("abrircarrito-mobile");
+btnabrircarrito.addEventListener("click", function () {
+  let navmobile = M.Sidenav.getInstance(document.querySelector("#mobile-demo"));
+  navmobile.close();
 });
 /*Ocultar el NavBar si se aprieta en seguir viendo*/
-var btncontinuarv = document.getElementById('seguirv_carrito');
-btncontinuarv.addEventListener('click', function () {
-    let carrito = M.Sidenav.getInstance(document.querySelector('#carrito'));
-    carrito.close();
+var btncontinuarv = document.getElementById("seguirv_carrito");
+btncontinuarv.addEventListener("click", function () {
+  let carrito = M.Sidenav.getInstance(document.querySelector("#carrito"));
+  carrito.close();
 });
 document.addEventListener("DOMContentLoaded", function () {
   var instance = M.Carousel.init({
     fullWidth: true,
   });
   var instances = M.Carousel.init(document.querySelectorAll(".carousel"));
-});
-
-// Or with jQuery
-
-$(document).ready(function () {
-  $(".carousel").carousel();
 });
