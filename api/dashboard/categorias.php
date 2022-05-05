54115bcbc1d393a91cb55769bf166e51d5b4d3f4
JonathanGdR <85887616+JonathanGdR@users.noreply.github.com>
2022-05-05 10:23:10 -0600

    Add files via upload
    Detalles pequeños

--- api/dashboard/categorias.php
+++ api/dashboard/categorias.php
@@ -53,7 +53,7 @@ if (isset($_GET['action'])) {
                             $result['exception'] = 'Nombres incorrectos';
                         } elseif ($categorias->createCategory()) {
                             $result['status'] = 1;
-                            $result['message'] = ' creado correctamente';
+                            $result['message'] = 'Categoría creada correctamente';
                         } else {
                             $result['exception'] = Database::getException();
                         }
@@ -68,7 +68,7 @@ if (isset($_GET['action'])) {
                                 $result['exception'] = 'Nombres invalido';
                             } elseif ($categorias->updateCategory()) {
                                 $result['status'] = 1;
-                                $result['message'] = 'Categoria modificado correctamente';
+                                $result['message'] = 'Categoría modificado correctamente';
                             } else {
                                 $result['exception'] = Database::getException();
                             }
@@ -91,6 +91,7 @@ if (isset($_GET['action'])) {
                                 $result['exception'] = 'Categoría inexistente';
                             } elseif ($categorias->deleteCategory()) {
                                 $result['status'] = 1;
+                                $result['message'] = 'Categoría eliminada correctamente';
                             } else {
                                 $result['exception'] = Database::getException();
                             }
