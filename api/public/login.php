3f8d689f6197797d8eb1368ac0ca10cfa840d493
JonathanGdR <85887616+JonathanGdR@users.noreply.github.com>
2022-05-15 14:40:53 -0600

    Add files via upload

--- api/public/login.php
+++ api/public/login.php
@@ -12,7 +12,7 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'session' => 0, 'message' => null, 'exception' => null, 'dataset' => null, 'username' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['id_clientes'])) {
+    if (isset($_SESSION['id_cliente'])) {
         $result['session'] = 1;
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
