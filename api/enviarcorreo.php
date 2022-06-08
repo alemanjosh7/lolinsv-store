03abd9e68a31452e086b1ad897fc9d66da4d66a1
DarkKingpro10 <84478517+DarkKingpro10@users.noreply.github.com>
2022-06-08 17:02:48 -0600

    Add files via upload

--- api/enviarcorreo.php
+++ api/enviarcorreo.php
@@ -73,7 +73,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'lolinsvoficial@gmail.com';                     //SMTP username
-    $mail->Password   = 'lolincontra';                               //SMTP password
+    $mail->Password   = 'evkhxyqlsvjoncyb';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
